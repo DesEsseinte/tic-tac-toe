8063f7ffbc132d764586a7a79814d7a3bc795bb2
robert <matthewrueter51@gmail.com>
2021-11-11 13:07:15 +0100

    refactored with module patterns

--- ALT-tic-tac-toe.js
+++ ALT-tic-tac-toe.js
@@ -1,75 +1,215 @@
-let gameBoard=['','','','','','','','',''];
+function onLoad(){
 
-
-//binds gameBoard array to display board on page.
-function createGameBoard(){
-  let i=0;
-  function iIncrement(){i++};
-  const square=document.querySelectorAll('.boardSquare');
-    square.forEach(item=>{
-      item.classList.add(i);
-      iIncrement();
+  const humanBTN=document.querySelector('#opponentHuman');
+    humanBTN.addEventListener('click', ()=>{
+      displayController.playHuman();
+      displayController.restart();
     })
-}
 
-
-
-//after clicking box this will update the array in the background.
-function updateBoardArray(sign){
-  const square=document.querySelectorAll('.boardSquare');
-    square.forEach(item=>{
-      item.addEventListener('click', ()=>{
-        let box=item.className[item.className.length-1]   //item.className #s only would be best
-        gameBoard[box]=sign
+  const computerEASY=document.querySelector('#easy');
+      computerEASY.addEventListener('click', ()=>{
+        console.log("I'm not yet programmed!");
       })
-    });
+
+  const computerHARD=document.querySelector('#hard');
+      computerHARD.addEventListener('click', ()=>{
+        console.log("I'm also not programmed!");
+      })
+
+  const startOverBTN=document.querySelector('#startOver');
+      startOverBTN.addEventListener('click', ()=>{
+        displayController.restart();
+      })
+
 }
 
 
 
-//after clicking box this will update the board as displayed on page.
-function displayBoard(sign){
-  let round=1;
-  const square= document.querySelectorAll('.boardSquare')
-    square.forEach(item => {
-      item.addEventListener('click', ()=>{
-        if(item.textContent==''){
-          if(round%2!=0 && item.textContent==''){
-            currentPlayer=playerOne;
-          }else{currentPlayer=playerTwo}
-          item.textContent=currentPlayer.sign;
-          round++;
+
+
+
+                                          ///////player creation///////////
+
+  const playerFactory=(sign)=>{
+    return {sign}
+  }
+      const player1=playerFactory('X');
+      const player2=playerFactory('O');
+
+
+
+
+
+
+
+            /////////// creates gameboard array and "attaches it" to DOM(classnames added 1-9).///////////
+
+  const gameBoard=(function () {
+
+    let board=[
+      ['0','1','2'],
+      ['3','4','5'],
+      ['6','7','8'],
+      ['0','3','6'],
+      ['1','4','7'],
+      ['2','5','8'],
+      ['2','4','6'],
+      ['0','4','8'],
+    ]
+    function createsGameBoard () {
+      let i=0;
+      function increment(){i++};
+      const square=document.querySelectorAll('.boardSquare');
+        square.forEach(item=>{
+          item.classList.add(i);
+          item.textContent='';
+          increment();
+        });
+    }
+
+      return{
+        board,
+
+        createBoard:createsGameBoard,
+
+        resetBoard:function(){
+          gameBoard.board=  [
+              ['0','1','2'],
+              ['3','4','5'],
+              ['6','7','8'],
+              ['0','3','6'],
+              ['1','4','7'],
+              ['2','5','8'],
+              ['2','4','6'],
+              ['0','4','8'],
+            ]
+          return board;
         }
+      }
+  })();
+
+
+
+
+
+
+
+
+
+
+                                  ///////what happens upon interacting with GUI buttons and clicks/////////
+      const displayController=(function() {
+
+        const playHuman=()=>{
+          gameBoard.createBoard();
+          const square=document.querySelectorAll('.boardSquare');
+            square.forEach(item=>{
+              item.addEventListener('click', ()=>{
+                if(item.textContent==''){
+                gameFlow.markSquare(item);
+                }
+              })
+            })
+          }
+
+        const restart=()=>{
+          gameBoard.createBoard();
+          gameBoard.resetBoard();
+          gameFlow.roundReset();
+          const message=document.querySelector('#messageDisplay');
+          message.textContent='';
+        }
+
+
+        return{
+          playHuman,
+          restart,
+        }
+      })();
+
+
+
+
+
+
+
+
+
+          ///////functions to control game flow: round#, which sign to place, check for a winner, etc..///////////////
+
+
+    const gameFlow=(function (){
+
+      let round=1;
+      let currentPlayer;
+
+      const markSquare=(item)=>{
+        round%2==0? currentPlayer=player2: currentPlayer=player1;
+        item.textContent=currentPlayer.sign;
+        updateBoardArray(item);
+        endGame.checkWin(currentPlayer);
+        endGame.tie();
+        round++;
+
+        return{round};
+      }
+
+      function updateBoardArray(item){
+        let boxNumber=item.className[item.className.length-1];   //0 for example.
+        gameBoard.board.forEach(index =>{
+          for(i=0;i<=2;i++){
+            if(index[i]==boxNumber){index[i]=currentPlayer.sign}
+          }
+        })
+      }
+
+
+      function roundReset(){
+        round = 1;
+      }
+
+      return{
+        round,
+        markSquare,
+        roundReset,
+      }
+    })();
+
+
+
+
+
+
+                          ////////////code which evaluates to see if winner or tie.//////////
+
+const endGame=(function() {
+  const message=document.querySelector('#messageDisplay');
+
+    function checkWin(currentPlayer){
+      gameBoard.board.forEach(item => {
+        let counter=0;
+        for(i=0;i<=2;i++){
+          if(item[i]==currentPlayer.sign){counter +=1}
+          if(counter==3){message.textContent=currentPlayer.sign + " wins!"}
+          }
+        })
+    }
+
+    function tie(){
+      let counter=0;
+      gameBoard.board.forEach(item =>{
+          for(c=0;c<=2;c++){
+            if(item[c]=='X' || item[c]=='O'){counter +=1}
+            }
       })
-    })
-}
+      if(counter==24){message.textContent="Tie."}
+    }
+
+      return{
+        checkWin,
+        tie
+      }
+})();
 
 
-
-
-//build funcitons into this to allow playerOne.updateBoard('x')...etc)
-function playerFactory(sign){
-  displayBoard(sign);
-  updateBoardArray(sign);
-return [displayBoard,updateBoardArray]
-
-
-}
-
-
-
-
-
-
-function startGame(){
-  createGameBoard();
-  const playerOne = playerFactory('X');
-  const playerTwo = playerFactory('O');
-
-
-
-}
-const opponentHuman=document.querySelector('#opponentHuman');
-  opponentHuman.addEventListener('click', ()=>{
-    startGame();
-  });
+onLoad();
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,123 +1,215 @@
-   const createGameBoard =()=>{
-      let i=0;
-      function iIncrement(){i++};
-      const gameBoard=['','X','','','O','','','',''];
-      const square=document.querySelectorAll('.boardSquare');
-      square.forEach(item => {
-        item.classList.add(i);
-        iIncrement();
+function onLoad(){
+
+  const humanBTN=document.querySelector('#opponentHuman');
+    humanBTN.addEventListener('click', ()=>{
+      displayController.playHuman();
+      displayController.restart();
+    })
+
+  const computerEASY=document.querySelector('#easy');
+      computerEASY.addEventListener('click', ()=>{
+        console.log("I'm not yet programmed!");
       })
 
-    };
-//when game-type is choosen and game begins (click event)
-    function startGame(){
+  const computerHARD=document.querySelector('#hard');
+      computerHARD.addEventListener('click', ()=>{
+        console.log("I'm also not programmed!");
+      })
 
-    }
+  const startOverBTN=document.querySelector('#startOver');
+      startOverBTN.addEventListener('click', ()=>{
+        displayController.restart();
+      })
 
-
-
-
-
-     //Code to set up human vs human game and "reset".
-
-     const playerFactory=(sign)=>{
-        const chooseBox=()=>{
-        let round=1;
-        const square=document.querySelectorAll('.boardSquare');
-        square.forEach(item => {
-          item.addEventListener('click', ()=>{
-          if(item.textContent==''){
-            if(round%2!=0 && item.textContent==''){
-              currentPlayer=playerOne;
-            }else{currentPlayer=playerTwo}
-            item.textContent=currentPlayer.sign;
-            round++;
-          }
-          });
-        });
-      }
-      return{sign,chooseBox};
-    }
-
-/*
-    const playerFactory=(sign)=>{
-
-      let gameBoard=['','','','','','','','',''];
-        const createGameBoard =()=>{
-          let i=0;
-          function iIncrement(){i++};
-          const square=document.querySelectorAll('.boardSquare');
-          square.forEach(item => {
-            item.textContent=gameBoard[i];
-            iIncrement();
-          })
-        };
-
-
-      const chooseBox=()=>{
-       let round=1;
-       const square=document.querySelectorAll('.boardSquare');
-       square.forEach(item => {
-         item.addEventListener('click', ()=>{
-           gameBoard[item]=playerOne.sign;
-           createGameBoard();
-         });
-       });
-     }
-     return{sign,chooseBox,createGameBoard,gameBoard};
-
-   }
-*/
-
-  const playerOne = playerFactory('X');
-  const playerTwo = playerFactory('O');
-
-
-const opponentHuman=document.querySelector('#opponentHuman');
-  opponentHuman.addEventListener('click', ()=>{
-    playerOne.chooseBox();
-  });
-const startOverBtn=document.querySelector('#startOver');
-  startOverBtn.addEventListener('click', ()=>{
-    const square=document.querySelectorAll('.boardSquare');
-    square.forEach(item => {
-      item.textContent='';
-    })
-  });
+}
 
 
 
 
 
-        //setting up computer-easy functionality.
-/*
+
+                                          ///////player creation///////////
+
   const playerFactory=(sign)=>{
-    const chooseBox=()=>{
-      let round=1;
-      const square=document.querySelectorAll('.boardSquare');
-      square.forEach(item => {
-        item.addEventListener('click', ()=>{
-          item.textContent=playerOne.sign;
-          round++;
-          n=3;
-          item.textContent=playerTwo.sign;
-        });
-      });
-    }
-    return{sign,chooseBox};
+    return {sign}
   }
+      const player1=playerFactory('X');
+      const player2=playerFactory('O');
 
 
 
 
-  const playerOne = playerFactory('X');
-  const playerTwo = playerFactory('O');
-*/
+
+
+
+            /////////// creates gameboard array and "attaches it" to DOM(classnames added 1-9).///////////
+
+  const gameBoard=(function () {
+
+    let board=[
+      ['0','1','2'],
+      ['3','4','5'],
+      ['6','7','8'],
+      ['0','3','6'],
+      ['1','4','7'],
+      ['2','5','8'],
+      ['2','4','6'],
+      ['0','4','8'],
+    ]
+    function createsGameBoard () {
+      let i=0;
+      function increment(){i++};
+      const square=document.querySelectorAll('.boardSquare');
+        square.forEach(item=>{
+          item.classList.add(i);
+          item.textContent='';
+          increment();
+        });
+    }
+
+      return{
+        board,
+
+        createBoard:createsGameBoard,
+
+        resetBoard:function(){
+          gameBoard.board=  [
+              ['0','1','2'],
+              ['3','4','5'],
+              ['6','7','8'],
+              ['0','3','6'],
+              ['1','4','7'],
+              ['2','5','8'],
+              ['2','4','6'],
+              ['0','4','8'],
+            ]
+          return board;
+        }
+      }
+  })();
 
 
 
 
-const easyBtn=document.querySelector('#easy');
-  easyBtn.addEventListener('click', () => {
-      playerOne.chooseBox();
-  });
+
+
+
+
+
+
+                                  ///////what happens upon interacting with GUI buttons and clicks/////////
+      const displayController=(function() {
+
+        const playHuman=()=>{
+          gameBoard.createBoard();
+          const square=document.querySelectorAll('.boardSquare');
+            square.forEach(item=>{
+              item.addEventListener('click', ()=>{
+                if(item.textContent==''){
+                gameFlow.markSquare(item);
+                }
+              })
+            })
+          }
+
+        const restart=()=>{
+          gameBoard.createBoard();
+          gameBoard.resetBoard();
+          gameFlow.roundReset();
+          const message=document.querySelector('#messageDisplay');
+          message.textContent='';
+        }
+
+
+        return{
+          playHuman,
+          restart,
+        }
+      })();
+
+
+
+
+
+
+
+
+
+          ///////functions to control game flow: round#, which sign to place, check for a winner, etc..///////////////
+
+
+    const gameFlow=(function (){
+
+      let round=1;
+      let currentPlayer;
+
+      const markSquare=(item)=>{
+        round%2==0? currentPlayer=player2: currentPlayer=player1;
+        item.textContent=currentPlayer.sign;
+        updateBoardArray(item);
+        endGame.checkWin(currentPlayer);
+        endGame.tie();
+        round++;
+
+        return{round};
+      }
+
+      function updateBoardArray(item){
+        let boxNumber=item.className[item.className.length-1];   //0 for example.
+        gameBoard.board.forEach(index =>{
+          for(i=0;i<=2;i++){
+            if(index[i]==boxNumber){index[i]=currentPlayer.sign}
+          }
+        })
+      }
+
+
+      function roundReset(){
+        round = 1;
+      }
+
+      return{
+        round,
+        markSquare,
+        roundReset,
+      }
+    })();
+
+
+
+
+
+
+                          ////////////code which evaluates to see if winner or tie.//////////
+
+const endGame=(function() {
+  const message=document.querySelector('#messageDisplay');
+
+    function checkWin(currentPlayer){
+      gameBoard.board.forEach(item => {
+        let counter=0;
+        for(i=0;i<=2;i++){
+          if(item[i]==currentPlayer.sign){counter +=1}
+          if(counter==3){message.textContent=currentPlayer.sign + " wins!"}
+          }
+        })
+    }
+
+    function tie(){
+      let counter=0;
+      gameBoard.board.forEach(item =>{
+          for(c=0;c<=2;c++){
+            if(item[c]=='X' || item[c]=='O'){counter +=1}
+            }
+      })
+      if(counter==24){message.textContent="Tie."}
+    }
+
+      return{
+        checkWin,
+        tie
+      }
+})();
+
+
+onLoad();
